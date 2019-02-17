18076db744bffa9fd400ad72661bb4b60975b600
Katarzynagalkina <ketty.galkina@gmail.com>
2019-02-17 22:35:41 +0300

    code refactoring

--- Galkina/Lab1/Main.java
+++ Galkina/Lab1/Main.java
@@ -1,12 +1,12 @@
+package Lab1;
+
 import javax.swing.*;
-import java.applet.Applet;
 import java.awt.*;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom. AffineTransform;
-import java.awt.geom.Ellipse2D;
-public class Main{
+
+public class Main {
 
     private static final int WIDTH = 1000;
     private static final int HEIGHT = 500;
@@ -16,11 +16,10 @@ public class Main{
     private static Color figureColor;
     private static Color borderColor;
     private static int borderWidth;
+    private static int angle = 1, step = 1, x = 0;
+    private static Figure figure = new Figure(angle, step);
 
-    private  static int angle=1,step=1, x = 0;
-
-    public static void main(String[] args)  {
-
+    public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setTitle("Animation");
@@ -28,42 +27,37 @@ public class Main{
         Dimension dimension = toolkit.getScreenSize();
         frame.setBounds(dimension.width / 6, dimension.height / 5, WIDTH, HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        Figure figure=new Figure(angle,step);
-        borderColor=Color.black;
-        figureColor=Color.lightGray;
-        borderWidth=3;
-
+        borderColor = Color.black;
+        figureColor = Color.lightGray;
+        borderWidth = 3;
         JPanel panel = new JPanel() {
             @Override
-             protected void paintComponent(Graphics g) {
+            protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 setBackground(Color.pink);
-                Graphics2D g2d = (Graphics2D)g;
-                g2d.drawLine(xBegin,Main.y,xEnd, Main.y);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.drawLine(xBegin, Main.y, xEnd, Main.y);
                 g2d.setColor(borderColor);
                 g2d.setStroke(new BasicStroke(borderWidth));
-                g2d.draw(figure.changeAngle(angle,x));
+                g2d.draw(figure);
                 g2d.setColor(figureColor);
-                g2d.fill(figure.changeAngle(angle,x));
-                }
-            };
+                g2d.fill(figure);
+            }
+        };
         frame.add(panel);
-
         Timer timer = new Timer(25, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(!(angle>=360))
-                    angle+=5;
-                else angle=1;
-                x+=step;
-                if(x==400 || x ==0)
-                    step=-step;
+                angle = (angle >= 360) ? 1 : angle + 5;
+                x += step;
+                if (x == 400 || x == 0) {
+                    step = -step;
+                }
+                figure = figure.changeAngle(angle, x);
                 panel.repaint();
             }
         });
         timer.start();
-        }
-};
+    }};
 
 
